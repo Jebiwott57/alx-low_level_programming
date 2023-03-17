a4534cf201468d67e58beec27d93e76e84b55b8b
Jebiwott57 <terikij@gmail.com>
2023-03-17 01:07:30 +0000

    c programming code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * Hello, world - print "Sentence"
- * main - empty (success)
- * return value = 0 (end)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
