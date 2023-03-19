f5c2d5fad0d1bc4c31ce3213fa09399a4e2925b3
Jebiwott57 <terikij@gmail.com>
2023-03-19 01:03:13 +0000

    incrementing numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	putchar(remove q and e from the string except ch);
+	}
+	putchar('\n');
+	return (0);
+}
