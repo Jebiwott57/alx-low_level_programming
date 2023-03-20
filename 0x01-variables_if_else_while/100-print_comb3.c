1e337835ea47e82688580bd7f91bcd9abcef6b1d
Jebiwott57 <terikij@gmail.com>
2023-03-20 13:48:51 +0000

    two digit combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,22 +14,18 @@ int main(void)
 	{
 		for (j = '1'; j <= '9'; j++)
 		{
-			if (i == j)
-			{
+			if (i >= j)
+				{
 				continue;
-			}
-			{
+				}
 			putchar(i);
 			putchar(j);
-			}
 			if (i == '8' && j == '9')
 			{
 				continue;
 			}
-			{
 			putchar(',');
 			putchar(' ');
-			}
 		}
 	}
 	putchar('\n');
