196398776ff6fa39f979bb1d08522e630a9a97cf
Jebiwott57 <terikij@gmail.com>
2023-03-24 10:25:44 +0000

    size of a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,17 +3,26 @@
 /**
  * print_square - print the square of a character
  *
+ * @size: size of the square
  * Return: 0 (Success)
  */
 void print_square(int size)
 {
-	int q;
-
-	q = size**2;
-		_putchar(q);
-		_putchar('\n');
-	if (size == 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
+	}
+	else
+	{
+		int l, j;
+
+		for (l = 0; l < size; l++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
 	}
 }
