79884fa8d12f31679bb1426c950d752a4b81fff4
Jebiwott57 <terikij@gmail.com>
2023-03-24 11:17:38 +0000

    print numbers from zero to fourteen

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,19 +7,18 @@
  */
 void more_numbers(void)
 {
-	int n;
+	int i, n;
 
-	int i = 0;
-
-	for (i = '1'; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-	if (n <= 14)
-	{
-		for (;n <= 14; n++)
-				{
-		_putchar(n);
-	
-	}
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+			{
+				_putchar(n / 10 + '0');
+			}
+				_putchar(n % 10 + '0');
+		}
 		_putchar('\n');
 	}
 }
