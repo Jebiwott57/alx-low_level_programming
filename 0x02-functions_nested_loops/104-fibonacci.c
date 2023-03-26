b07f117e06205ee94d98e4450c59c67628087f48
Jebiwott57 <terikij@gmail.com>
2023-03-26 15:26:01 +0000

    print first 98 fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - print the first 100 fibonacci numbers
- *
+ * main - print the first 103 fibonacci numbers
+ +
+ i*
  * Return: 0 (Success)
  */
 int main(void)
 {
 	long int i = 0, j = 1, k, cnt;
 
-	for (cnt = 3; cnt <= 100; cnt++)
+	for (cnt = 3; cnt <= 103; cnt++)
 	{
 		k = i + j;
 		printf("%lu", k);
 		i = j;
 		j = k;
 
-		if (k != (cnt != 100))
+		if (cnt != 103)
 		{
 		printf(", ");
 		}
