a0650225d1876c68644db77e67d01b75607612d2
Jebiwott57 <terikij@gmail.com>
2023-03-27 17:01:44 +0000

    takes a pointer to an int

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,28 +14,39 @@ void print_times_table(int n)
 	{
 	for (a = 0; a <= n; a++)
 	{
-	_putchar('0');
-	for (b = 1; b <= n; b++)
+	for (b = 0; b <= n; b++)
 	{
-		_putchar(',');
-		_putchar(' ');
-		c = a * b;
-		if (c <= 99)
-		_putchar(' ');
-		if (c <= 9)
-		_putchar(' ');
-		if (c >= 100)
+		c = b * a;
+		if (b == 0)
 		{
-			_putchar((c / 100) + '0');
-			_putchar((c / 100) % 10 + '0');
+			_putchar(c + '0');
 		}
-			else if (c <= 99 && c >= 10)
+			else if (c < 10 && b != 0)
 			{
-			_putchar((c / 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
 			}
-			_putchar((c / 10) + '0');
+			else if (c >= 10 && c < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
 			}
-			_putchar('\n');
+			else if (c >= 100)
+			{
+			_putchar(',');
+			_putchar(' ');
+			_putchar((c / 10) + '0');
+			_putchar(((c / 10) % 10) + '0');
+			_putchar((c % 10) + '0');
+			}
+		}
 	}
 	}
+		_putchar('\n');
 }
--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,17 +7,15 @@
  */
 int main(void)
 {
-	int i, sum;
+	int i, sum = 0;
 
 	for (i = 1; i <= 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{
-		sum = 0;
-
 		(sum + i) == sum;
-		printf("%d\n", sum);
 		sum = i;
+		printf("%d\n", sum);
 		}
 	}
 	return (0);
