07b2042ad17c9f3cc659ef18a05d6bd564ab7ad7
Jebiwott57 <terikij@gmail.com>
2023-03-27 18:14:53 +0000

    length of a string with next line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - print a string to stdout.
+ * @str: string to print
+ *
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+	-putchar(*str++);
+	}
+	_putchar('\n');
+}
