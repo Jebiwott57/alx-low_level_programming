cc4967f466f73219cfdc98aee0bc484e59ac4589
Jebiwott57 <terikij@gmail.com>
2023-04-01 17:47:24 +0000

    print first 98 fibonacci numners

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,31 +8,41 @@
  */
 int main(void)
 {
-	long int j = 1, k = 2, j1, j2, k1, k2;
-
+	unsigned long j = 0, k = 1, sum;
+	unsigned long j1, j2, k1, k2;
+	unsigned long h1, h2;
 	int cnt;
 
-	printf("%lu", j);
-
-	for (cnt = 1; cnt <= 91; cnt++)
+	for (cnt = 0; cnt <= 92; cnt++)
 	{
-		printf(", %lu", k);
-		k = k + j;
-		j = k - j;
+		sum = j + k;
+		printf("%lu, ", sum);
+
+		j = k;
+		k = sum;
 	}
 	j1 = j / 1000000000;
 	j2 = j % 1000000000;
 	k1 = k / 1000000000;
 	k2 = k % 1000000000;
 
-	for (cnt = 92; cnt < 99; ++cnt)
+	for (cnt = 93; cnt < 99; ++cnt)
 	{
-	printf(", %lu", k1 + (k2 / 1000000000));
-	printf("%lu", k2 % 1000000000);
-	k1 = k1 + j1;
-	j1 = k1 - j1;
-	k2 = k2 + j2;
-	j2 = k2 - j2;
+		h1 = j1 + k1;
+		h2 = j2 + k2;
+		if (j1 + j2 > 999999999)
+		{
+			h1 += 1;
+			h2 %= 1000000000;
+		}
+	printf("%lu, %lu", h1, h2);
+	if (cnt != 98)
+		printf(", ");
+
+	j1 = k1;
+	j2 = k2;
+	k1 = h1;
+	k2 = h2;
 	}
 	printf("\n");
 	return (0);
