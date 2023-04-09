aa9a7cbc76127da1d97bbad35962b9a53e164692
Jebiwott57 <terikij@gmail.com>
2023-04-09 22:32:31 +0000

    Create a script called create_static_lib.sh that creates a static library called liball.a from all the .c files that are in the current directory.

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - Check if letter c is uppercase.
+ *
+ * @c: Check is the letter is uppercase.
+ * Return: 0 if Uppercase
+ */
+
+int _isupper(int c)
+{
+
+	return (c >= 'A' && c <= 'Z');
+}
