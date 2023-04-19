7a132c24a860dcc1f820083cc227e7a374f07687
Jebiwott57 <terikij@gmail.com>
2023-04-19 21:16:34 +0000

    Updated header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,12 @@
 #ifndef FUNCTION_POINTERS_HEADER_FILE
 #define FUNCTION_POINTERS_HEADER_FILE
 
+#include <stddef.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-
-#include <stddef.h>
-#include <stdlib.h>
 
 #endif
