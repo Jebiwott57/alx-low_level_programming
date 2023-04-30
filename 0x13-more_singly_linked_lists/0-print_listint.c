207eb77eb3d98d9a8ce9183459327431d1f7343c
Jebiwott57 <terikij@mail.com>
2023-04-30 10:40:20 +0000

    return all elements of a singly linked lisr listint_t

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t element = 0;
+	size_t element;
 
+	element = 0;
 	while (h != NULL)
 	{
 		printf("%d\n", (*h).n);
 	h = (*h).next;
 	element++;
 	}
-	return (element);
+return (element);
 }
